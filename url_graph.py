bc3a5a87a1950d7230743eb6ed541e5cc2c9257f
Timofey Asyrkin <tasyrkin@gmail.com>
2014-03-11 23:37:17 +0100

    UrlGraph and Url classes added

---
+++ url_graph.py
@@ -0,0 +1,34 @@
+
+
+class Url:
+
+	def __init__(self, name):
+		self.name = name
+		self.urls = list()
+
+	def add_url(self, url):
+		self.urls.append(url)
+	
+	def add_urls(self, urls):
+		self.urls.extend(urls)
+
+class UrlGraph:
+	
+	def __init__(self, url):
+		self.url = url
+
+	def get_url_by_name(self, url_name):
+		except NotImplementedError()
+
+NAME_PATTERN_URLS_GRAPH = 'urls_graph_'
+class FilePersister:
+	def __init__(self, folder):
+		self.folder = folder
+	
+	def get_url_graph(self):
+		
+		except NotImplementedError()
+
+	def save_url_graph(self, url_graph):
+		except NotImplementedError()
+
