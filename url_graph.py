02119e6b85f203eabade441adc773a4d2a59b5fd
Timofey Asyrkin <tasyrkin@gmail.com>
2014-03-15 20:18:22 +0100

    Started implementation of url graph persistance

--- url_graph.py
+++ url_graph.py
@@ -1,4 +1,5 @@
-
+import os
+import fileinput
 
 class UrlNode:
 
@@ -16,17 +17,30 @@ class UrlGraph:
 	init_url_node = None
 
 	def __init__(self, url_node):
+		pass
+
+	def __init__(self, url_node):
 		self.init_url_node = url_node
 
 	def find_url_node_by_name(self, url_node):
 		except NotImplementedError()
 
 class UrlGraphFileSystemPersister:
+	NAME_PATTERN_URL_GRAPH = 'url_graph_'
+
 	def __init__(self, folder):
 		self.folder = folder
 	
 	def load_url_graph(self):
-		except NotImplementedError()		
+		url_graph_fnames = [fname 
+				for fname in os.listdir(folder) 
+				if fname.startswith(NAME_PATTERN_URL_GRAPH)]
+		if len(url_graph_fnames) == 0:
+			return UrlGraph()
+		if len(url_graph_fnames) > 1:
+			except ValueError(str.format('found multiple url graph files: {}', str(url_graph_fnames)))
+		for line in fileinput(url_graph_fnames[0]):
+
 
 	def save_url_graph(self, url_graph):
 		except NotImplementedError()
