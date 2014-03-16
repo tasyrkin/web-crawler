9843598f4f32a6ff24518ecb5303940fb64d00ac
Timofey Asyrkin <tasyrkin@gmail.com>
2014-03-16 02:03:38 +0100

    UrlGraphFileSystemPersister: implemented load_url_graph()

--- url_graph.py
+++ url_graph.py
@@ -39,8 +39,27 @@ class UrlGraphFileSystemPersister:
 			return UrlGraph()
 		if len(url_graph_fnames) > 1:
 			except ValueError(str.format('found multiple url graph files: {}', str(url_graph_fnames)))
+		
+		str_to_url_node = dict()
+		init_node = None
 		for line in fileinput(url_graph_fnames[0]):
+			url_to_neighbours = line.split(':')
+			url_str = url_to_neighbours[0]
+			neighbours_str = url_to_neighbours[1].split(',')
+			url_node = str_to_url_node.get(url_str)
+			if not url_node:
+				url_node = UrlNode(url_str)
+				str_to_url_node[url_str] = url_node
+			for neighbour_str in neighbours_str:
+				neighbour_node = str_to_url_node.get(neigbour_str)
+				if not neighbour_node:
+					neighbour_node = UrlNode(neighbour_str)
+					str_to_url_node[neighbour_str] = neighbour_node
+				url_node.add_url_node(neighbour_node)
+			if not init_node:
+				init_node = url_node
 
+		return url_graph(init_node)
 
 	def save_url_graph(self, url_graph):
 		except NotImplementedError()
